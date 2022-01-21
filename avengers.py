8821f1ea4e69a87dcc355ac9577de2f92350816d
LukasSjoberg <91466081+LukasSjoberg@users.noreply.github.com>
2022-01-21 11:11:32 +0100

    Update avengers.py

--- avengers.py
+++ avengers.py
@@ -1,8 +1,11 @@
+import random
+
 list1=["spider", "iron", "the", "black", "star", "berry", "super", "thor", "green", "captain",]
 
 list2 =["man", "man", "man", "Allan", "lord", "woman", "hulk"]
 
 
-print(random.choice(list1))
 
+
+print(random.choice(list1),random.choice(list2))
 
