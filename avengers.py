bd7fe107bb1967ac75bfe3218af716f2b79e315d
LukasSjoberg <91466081+LukasSjoberg@users.noreply.github.com>
2022-01-21 11:15:15 +0100

    Update avengers.py

--- avengers.py
+++ avengers.py
@@ -1,11 +1,12 @@
 import random
-
+import time
 list1=["spider", "iron", "the", "black", "star", "berry", "super", "thor", "green", "captain",]
 
 list2 =["man", "man", "man", "Allan", "lord", "woman", "hulk"]
 
 
 
+while True:
+    print(random.choice(list1),random.choice(list2))
 
-print(random.choice(list1),random.choice(list2))
-
+    time.sleep(0.2)
