7639fbd2214f6e73b5bc1a7c61ab48e599f2527b
LukasSjoberg <91466081+LukasSjoberg@users.noreply.github.com>
2022-02-25 09:58:01 +0100

    Create flödes.py

---
+++ flödes.py
@@ -0,0 +1,32 @@
+print('skriv din nya gren:')
+x = input()
+print(x)
+
+a = input('är det en sport?:')
+
+if a == "ja":
+  print("bra")
+else :
+  print("skit it")
+  exit()
+
+b = input('e den inkluderande?:')
+
+if b == "ja":
+    print("illgött")
+else:
+    print("lös de")
+
+c = input('är de underhållande?:')
+
+if c == "ja":
+    print("illgött")
+else:
+    print("lös de")
+
+d = input('är den känd eller finns den över iprinsip över hela världen?:')
+
+if d == "ja":
+    print("illgött")
+else:
+    print("sprid som corona")
